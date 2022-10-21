73977d3007593c67b2964fb92acd5d36beabf11f
Victor Valentin <31552722+VictorHugoValentin@users.noreply.github.com>
2022-10-21 12:58:55 -0300

    [21/10/22] [1.12.0] Implementación Inicial de SSLHandler y vista ejemplo.

---
+++ Vista/SSLHandler.Ejemplo.php
@@ -0,0 +1,81 @@
+<?php
+
+include_once '../vendor/autoload.php';
+
+/* 
+
+Este es un ejemplo de uso de la Clase \Uargflow\EncryptDecryptData.
+
+*/
+
+// Casos de test
+$data = "new \Uargflow\SessionManager()";
+
+$ArrayData['1']  = "Eder";
+$ArrayData['2'] = "Victor";
+$ArrayData['3'] = "Franco";
+
+//session_destroy();
+?>
+
+<html>
+
+<head>
+    <?php include_once '../lib/includesCss.php'; ?>
+    <?php include_once '../lib/includesJs.php'; ?>
+    <?php include_once '../lib/Constantes.Class.php'; ?>
+    <title><?= Constantes::NOMBRE_SISTEMA; ?> - SSL Handler</title>
+</head>
+
+<body>
+
+    <?php include_once '../gui/navbar.php'; ?>
+    <div class="container-fluid">
+        <div class="card ">
+            <div class="card-header">
+                <h5 class="card-title">
+                    <i class="oi oi-thumb-up"> </i> Ejemplo de uso - componente \Uargflow\SSLHandler
+                </h5>
+            </div>
+            <div class="card-body">
+                <table id="csvtable" class="table table-striped table-hover table-responsive-sm table-sm btn-lg">
+                    <thead>
+                        <tr class="table-info">
+                            <th>Texto Plano</th>
+                            <th>Encriptado</th>
+                            <th>Descifrado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($ArrayData as $Item) {
+                        ?>
+                            <tr>
+                                <td><?= $Item; ?></td>
+                                <td><?php $a = \Uargflow\SSLHandler::encrypt($Item); echo $a?></td>
+                                <td><?= \Uargflow\SSLHandler::decrypt($a); ?></td>
+                            </tr>
+                        <?php }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-body jumbotron">
+                <h3><i class="oi oi-thumb-up"> </i> Ayuda para Desarrolladores</h3>
+                <hr />
+                <p>La clase SSLHandler se encuentra en el namespace \Uargflow, e implementa la interface SSLHandlerInterface.</p>
+                <p>Esta clase permite encriptar y descifrar datos mediante la libreria OpenSSL y un vector de inicializac&oacute;n pseudorandom.</p>
+                <p>Para mayor informacion, visitar https://www.php.net/manual/en/refs.crypto.php</p>
+                <pre>
+                    <code class="language-html" data-lang="html">
+                    $encriptado = \Uargflow\SSLHandler::encrypt($cadenaDeTexto);
+                    $descifrado = \Uargflow\SSLHandler::decrypt($encriptado);
+                    </code>
+                </pre>
+
+            </div>
+        </div>
+    </div>
+    <?php include_once "../gui/footer.php"; ?>
+</body>
+
+</html>
