bddc89c3596b6dd821103543ccb54a07ce132620
Eder dos Santos <eder.santos@gmail.com>
2022-11-04 11:14:32 -0300

    [04/11/2022] [1.17.0] Refactoring include_once en Index (Incorpora directiva __DIR__).

--- Vista/index.php
+++ Vista/index.php
@@ -1,32 +1,22 @@
-<?php
-include_once '../vendor/autoload.php';
-?>
+<?php include_once __DIR__ . '/../vendor/autoload.php'; ?>
 
 <html>
-
 <head>
-    <?php include_once '../lib/includesCss.php'; ?>
-    <?php include_once '../lib/includesJs.php'; ?>
-    <?php include_once '../lib/Constantes.Class.php'; ?>
+    <?php include_once __DIR__ . '/../lib/includesCss.php'; ?>
+    <?php include_once __DIR__ . '/../lib/includesJs.php'; ?>
+    <?php include_once __DIR__ . '/../lib/Constantes.Class.php'; ?>
     <title><?= Constantes::NOMBRE_SISTEMA; ?> - Portada</title>
 </head>
 
 <body>
-    <?php include_once '../gui/navbar.php'; ?>
-
+    <?php include_once __DIR__ . '/../gui/navbar.php'; ?>
     <div class="jumbotron bg-warning">
         <h3 class="display-4"><i class="bi-check-square"></i> <?= Constantes::NOMBRE_SISTEMA ?></h3>
         <p class="lead">Hola, Valeria!</p>
         <p class="lead">Ingrese al sistema que desea a continuaci&oacute;n.</p>
     </div>
-
-
-
-    <!-- </div> -->
-
     <?php include './index.jumbo.php' ?>
     <br />
-    <?php include_once '../gui/footer.php'; ?>
+    <?php include_once __DIR__ . '/../gui/footer.php'; ?>
 </body>
-
 </html>
