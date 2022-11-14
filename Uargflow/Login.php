2babe2db56c32dad40061506e3e2c95a10053b15
BracamonteF <bracamontefacundo@agvp.gob.ar>
2022-11-14 14:55:41 -0300

    [14/11/2022][1.14.0] Incio de procesos de autetificacion.

--- Uargflow/Login.php
+++ Uargflow/Login.php
@@ -3,7 +3,24 @@
 namespace Uargflow;
 
 class Login {
-    public function findUsuario($usuario){
-        
+    
+    /**
+     *
+     * @return  bool
+     */ 
+    public function verificaUsuario($usuario){
+        $mapper = new \Mappers\Usuario();
+        return(!empty($mapper->findbyNombreUsuario($usuario)));
+    }
+
+    /**
+     *
+     * @return  bool
+     */ 
+    public function verificaPass($pass, $usuario){
+        $mapper = new \Mappers\Usuario();
+        $objeto = new \Modelo\Usuario($mapper->findbyNombreUsuario($usuario));
+        return Hash::verificaPasswordHashBD($pass, $objeto->getPassword());
     }
 }
+?>
